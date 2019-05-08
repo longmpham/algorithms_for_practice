ead0a326808c70811b26c62e18a8248ce0d217c2
Long <longish222@gmail.com>
2019-05-07 22:06:03 -0400

    added more array stuff and string stuff

--- algorithms/arrays/searchInsert.py
+++ algorithms/arrays/searchInsert.py
@@ -15,11 +15,13 @@ def searchInsert(array, target):
 			index += 1
 
 def searchInsert_v2(nums, target):
+	# we can optimize by cutting work in half.
+
+	# bonus: we can cut work in half multiple times until the value is found!
+	# 		if its not found...
 
 	index = 0
 	half = int(len(nums)/2)
-	print(half)
-	print(len(nums))
 
 	if nums[half] == target: return half
 
@@ -47,8 +49,27 @@ def searchInsert_v2(nums, target):
 
 		return half
 
+
+
+def searchInsert_v3_recursive(nums, target):
+	
+	# DOES NOT WORK CURRENTLY.
+	half = int(len(nums)/2)
+
+	if nums[half] == target:
+		return half
+	elif target < nums[half]:
+		searchInsert_v3_recursive(nums[0:half], target)
+	else:
+		searchInsert_v3_recursive(nums[half:len(nums)+1], target)
+
+	return 'none found'
+
+
+
+
 array = [1,3,5,6]
-target = 5
+target = 6
 #print(searchInsert(array, target)) 
 
 print(searchInsert_v2(array, target)) 
